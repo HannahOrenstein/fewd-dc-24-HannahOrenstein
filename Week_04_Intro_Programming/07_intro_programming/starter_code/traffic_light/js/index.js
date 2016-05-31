03c42649302d9c9cbfe19e2dab7ed5d2c9eea1be
HannahOrenstein <Hannah Orenstein@hannahoomputer2.lan>
2016-05-31 08:39:38 -0400

    Week 4 Classwork

--- Week_04_Intro_Programming/07_intro_programming/starter_code/traffic_light/js/index.js
+++ Week_04_Intro_Programming/07_intro_programming/starter_code/traffic_light/js/index.js
@@ -1,10 +1,31 @@
+//Click on Stop
 document.getElementById('stopButton').onclick = illuminateRed;
+
+//Click on Slow
+document.getElementById('slowButton').onclick = illuminatePurple;
+
+//Click on Go
+document.getElementById('goButton').onclick = illuminateGreen;
+
 
 function illuminateRed() {
   clearLights();
   document.getElementById('stopLight').style.backgroundColor = "red";
 }
+	//Change top circle to red
 
 function clearLights() {
   document.getElementById('stopLight').style.backgroundColor = "black";
+  document.getElementById('slowLight').style.backgroundColor = "black";
+  document.getElementById('goLight').style.backgroundColor = "black";
+}
+
+function illuminatePurple() {
+  clearLights();
+  document.getElementById('slowLight').style.backgroundColor = "purple";
+}
+
+function illuminateGreen() {
+  clearLights();
+  document.getElementById('goLight').style.backgroundColor = "green";
 }
--- Week_04_Intro_Programming/08_Intro_jQuery/starter_code/traffic_light/js/index.js
+++ Week_04_Intro_Programming/08_Intro_jQuery/starter_code/traffic_light/js/index.js
@@ -1,1 +1,39 @@
 //Implement the red light using jQuery. Don't forget to add the script tags.
+
+
+//Click on Stop
+jQuery("#stopButton").on("click",illuminateRed);
+
+//Click on Slow
+jQuery("#slowButton").on("click",illuminatePurple);
+/*document.getElementById('slowButton').onclick = illuminatePurple;*/
+
+//Click on Go
+jQuery("#goButton").on("click",illuminateGreen);
+/*document.getElementById('goButton').onclick = illuminateGreen;*/
+
+
+function illuminateRed() {
+  clearLights();
+  jQuery("#stopLight").css("background-color", "red");
+  /*document.getElementById('stopLight').style.backgroundColor = "red";*/
+}
+	//Change top circle to red
+
+function clearLights() {
+  jQuery(".bulb").css("background-color", "black");
+  /*document.getElementById('stopLight').style.backgroundColor = "black";
+  document.getElementById('slowLight').style.backgroundColor = "black";
+  document.getElementById('goLight').style.backgroundColor = "black";*/
+}
+
+function illuminatePurple() {
+  clearLights();
+  jQuery("#slowLight").css("background-color", "purple");
+}
+
+function illuminateGreen() {
+  clearLights();
+  jQuery("#goLight").css("background-color", "green");
+}
+
