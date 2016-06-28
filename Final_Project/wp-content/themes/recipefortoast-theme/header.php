b0791118ef616caea2d9b81880f07d801d8a7068
HannahOrenstein <Hannah Orenstein@hannahoomputer2.lan>
2016-06-28 10:37:10 -0400

    Nav bar

--- Final_Project/wp-content/themes/recipefortoast-theme/header.php
+++ Final_Project/wp-content/themes/recipefortoast-theme/header.php
@@ -14,7 +14,22 @@
     <?php wp_head(); ?>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha256-KXn5puMvxCw+dAYznun+drMdG1IFl3agK0p/pqT9KAo= sha512-2e8qq0ETcfWRI4HJBzQiA3UoyFk6tbNyG+qSaIBZLyW9Xf3sWZHN/lxe9fTh1U45DpPf07yj94KsUHHWe4Yk1A==" crossorigin="anonymous"></script>
 </head>
+    <header>
+        <div class="chevron" "clearfix">
+            <nav>
+                <ul class="mainNavigation">
+                    <li><a href="http://www.recipefortoast.com/">Home</a></li>
+                    <li><a href="http://www.recipefortoast.com/recipes">Recipes</a></li>
+                    <li><a href="http://www.recipefortoast.com/about">About</a></li>
+                </ul>
+            </nav>
+        </div>
+    </header>
 <body <?php body_class(); ?>>
+
+
+
+
 
 
 
